2044589dbea4303c6c902ddd0e7fb24198d1b533
SueiWenChen <swc435@nyu.edu>
2026-06-06 16:09:42 +0200

    changed torch.dataloader

--- Project3/source/cvrp.py
+++ Project3/source/cvrp.py
@@ -30,107 +30,248 @@ THE SOFTWARE.
 # EXTERNAL LIBRARY
 import numpy as np
 from numpy.random._generator import Generator
-from torch.utils.data import Dataset
-from torch.utils.data import DataLoader
+from torch.utils.data import Dataset, DataLoader
 from IPython.core.debugger import set_trace
 import torch
-from typing import List, Optional
-from .parameters import *
-from .cvrp_generator import CVRPGenerator
+from typing import List, Optional, Union
 
+from .parameters import MIN_NUM_CUSTOMERS, MAX_NUM_CUSTOMERS, MAX_DEMAND, VEHICLE_CAPACITY
+
+from .cvrp_generator import CVRPGenerator
+NumberType = np.generic | int | float
 DEVICE = None # to be set in the notebook before using
 
-NumberType = np.generic | int | float
-####################################
-# DATA LOADER
-####################################
-def CVRP_DATA_LOADER__RANDOM(num_sample: int, batch_size: int, 
-                             problem_sizes_mean: Optional[List[NumberType]|NumberType]=None, 
-                             problem_sizes_std: Optional[List[NumberType]|NumberType]=None, 
-                             rng: Generator=None):
-    ''' Data loader that generates random CVRP instances on-the-fly.
-        The problem sizes (number of customers) are fixed per batch and sampled 
-        from a gaussian with specified mean and std before rounding
-        Parameters:
-            num_sample: total number of data samples to generate
-            batch_size: batch size for the data loader
-            problem_sizes_mean: the means of problem size per batch; if None, set to 25
-            problem_sizes_std: the stds of problem size per batch; if None, set to 0
-            rng: random number generator for reproducibility
+def CVRP_DATA_LOADER(
+    generator: CVRPGenerator,
+    num_sample: int, 
+    batch_size: int, 
+    problem_sizes_mean: Optional[Union[List[NumberType], NumberType]] = None, 
+    problem_sizes_std: Optional[Union[List[NumberType], NumberType]] = None, 
+    normalized_coordinates: bool = True,
+    rng: Optional[np.random.Generator] = None
+):
     '''
-    # Input sanity checks
-    n_batches = int(np.ceil(num_sample/batch_size)) # number of batches
-    if isinstance(problem_sizes_mean, List):
-        assert len(problem_sizes_mean) == n_batches, \
-                "Length of problem_sizes_mean must match the number of batches = ceil(num_sample/batch_size)"
-        if isinstance(problem_sizes_std, List):
-            assert len(problem_sizes_mean) == len(problem_sizes_std), \
-                "problem_sizes_mean and problem_sizes_std must have the same length if provided as lists"
-    # Convert means and stds to list of length n_batches
-    problem_sizes_mean = 25 if problem_sizes_mean is None else problem_sizes_mean
-    problem_sizes_mean = [problem_sizes_mean]*n_batches if isinstance(problem_sizes_mean, NumberType) \
-                            else problem_sizes_mean
-    problem_sizes_std = 0 if problem_sizes_std is None else problem_sizes_std
-    problem_sizes_std = [problem_sizes_std]*n_batches if isinstance(problem_sizes_std, NumberType) \
-                            else problem_sizes_std
-    assert np.all(np.array(problem_sizes_std)>=0), "problem_sizes_std must be nonnegative or contain nonnegative entries"
+    Dataloader that generates random CVRP instances on-the-fly.
+    The problem sizes (number of customers) are fixed per batch and sampled 
+    from a gaussian with specified mean and std before rounding
+    Parameters:
+        generator: an instance of CVRPGenerator that provides the logic for sampling CVRP instances
+        num_sample: total number of instances to generate
+        batch_size: batch size for the data loader
+        problem_sizes_mean: the means of problem size per batch; if None, set to 25
+        problem_sizes_std: the stds of problem size per batch; if None, set to 0
+        rng: random number generator for reproducibility
+     '''
+    n_batches = int(np.ceil(num_sample / batch_size))
     
+    # Input sanity checks matching your template
+    if isinstance(problem_sizes_mean, list):
+        assert len(problem_sizes_mean) == n_batches, \
+            "Length of problem_sizes_mean must match the number of batches"
+        if isinstance(problem_sizes_std, list):
+            assert len(problem_sizes_mean) == len(problem_sizes_std), \
+                "problem_sizes_mean and problem_sizes_std must have the same length"
+                
+    problem_sizes_mean = 50 if problem_sizes_mean is None else problem_sizes_mean
+    problem_sizes_mean = [problem_sizes_mean] * n_batches if not isinstance(problem_sizes_mean, list) else problem_sizes_mean
+    
+    problem_sizes_std = 0 if problem_sizes_std is None else problem_sizes_std
+    problem_sizes_std = [problem_sizes_std] * n_batches if not isinstance(problem_sizes_std, list) else problem_sizes_std
+    
+    assert np.all(np.array(problem_sizes_std) >= 0), "problem_sizes_std must be nonnegative"
     
     rng = np.random.default_rng() if rng is None else rng
-    dataset = CVRP_Dataset__Random(num_sample=num_sample, 
-                                   batch_size=batch_size, 
-                                   problem_sizes_mean=problem_sizes_mean,
-                                   problem_sizes_std=problem_sizes_std,
-                                   rng=rng)
-    data_loader = DataLoader(dataset=dataset,
-                             batch_size=batch_size,
-                             shuffle=False,
-                             num_workers=0,
-                             collate_fn=CVRP_collate_fn)
+    
+    dataset = CVRP_Dataset(
+        generator=generator,
+        num_sample=num_sample, 
+        batch_size=batch_size, 
+        problem_sizes_mean=problem_sizes_mean,
+        problem_sizes_std=problem_sizes_std,
+        rng=rng
+    )
+    
+    data_loader = DataLoader(
+        dataset=dataset,
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=0,
+        collate_fn=CVRP_collate_fn
+    )
     return data_loader
 
-class CVRP_Dataset__Random(Dataset):
-    def __init__(self, num_sample, batch_size, problem_sizes_mean, problem_sizes_std, rng=None):
+
+
+class CVRP_Dataset(Dataset):
+    def __init__(self, 
+                 generator: CVRPGenerator, 
+                 num_sample: int, 
+                 batch_size: int, 
+                 problem_sizes_mean: List[NumberType], 
+                 problem_sizes_std: List[NumberType],
+                 rng=None):
+        '''Constructs a dataset of CVRP instances by pre-generating all the data in batches.
+        Parameters:
+            generator: an instance of CVRPGenerator that provides the logic for sampling CVRP instances
+            num_sample: total number of instances to generate
+            batch_size: batch size for the dataset
+            problem_sizes_mean: the means of problem size per batch
+            problem_sizes_std: the stds of problem size per batch
+            rng: random number generator for reproducibility
+        '''
+        self.generator = generator
         self.num_sample = num_sample
         self.batch_size = batch_size
         self.num_batches = int(np.ceil(num_sample / batch_size))
         self.rng = np.random.default_rng() if rng is None else rng
 
-        # Generate dynamic problem size (possible number of customers) per batch
-        # via guassian + clipping + rounding
+        # 1. Generate dynamic problem sizes per batch
         self.problem_size_list = np.round(self.rng.normal(problem_sizes_mean, problem_sizes_std))
         self.problem_size_list = np.clip(self.problem_size_list, MIN_NUM_CUSTOMERS, MAX_NUM_CUSTOMERS).astype(int)
+        
+        # 2. Pre-allocate storage lists for the batched data
+        self.batched_edge_ids = []
+        self.batched_fractions = []
+        self.batched_demands = []
+        self.batched_cost_matrix = []
 
-        self.demand_scalers_list = [VEHICLE_CAPACITY] * len(self.problem_size_list)
+        # 3. Use sample_batch to generate all data up front
+        for batch_idx in range(self.num_batches):
+            prob_size = self.problem_size_list[batch_idx]
+            num_locations = prob_size + 1  # 1 depot + customers
 
-    def __getitem__(self, index):
-        # determine the problem size and demand scaler for this index
-        batch_number = index // self.batch_size
-        problem_size = self.problem_size_list[batch_number]
-        demand_scaler = self.demand_scalers_list[batch_number]
-
-        # generate random data
-        depot_xy_data = self.rng.random((1, 2))
-        node_xy_data = self.rng.random((problem_size, 2))
-        node_demand_data = self.rng.integers(1, MAX_DEMAND_PER_CUSTOMER + 1, problem_size) / demand_scaler
-
-        return depot_xy_data, node_xy_data, node_demand_data, demand_scaler
+            # Vectorized generation of the entire batch at once
+            edge_idx, ts, demands, cost_matrices = self.generator.sample_batch(
+                batch_size=self.batch_size, 
+                num_locations=num_locations
+            )
+            self.batched_edge_ids.append(edge_idx)
+            self.batched_fractions.append(ts)
+            self.batched_demands.append(demands)
+            self.batched_cost_matrix.append(cost_matrices)
 
     def __len__(self):
         return self.num_sample
 
+    def __getitem__(self, index):
+        # Determine which batch the index belongs to, and its local position inside that batch
+        batch_number = index // self.batch_size
+        item_number = index % self.batch_size
 
+        # Extract the single item from our pre-allocated batch tensors
+        edge_ids = self.batched_edge_ids[batch_number][item_number]
+        fractions = self.batched_fractions[batch_number][item_number]
+        demands = self.batched_demands[batch_number][item_number]
+        cost_matrix = self.batched_cost_matrix[batch_number][item_number]
+
+        return edge_ids, fractions, demands, cost_matrix
+    
 def CVRP_collate_fn(batch):
-    depot_xy_data_tuples, node_xy_data_tuples, node_demand_data_tuples, demand_scalers = zip(*batch)
-    depot_xy_data = np.array(depot_xy_data_tuples)
-    node_xy_data = np.array(node_xy_data_tuples)
-    node_demand_data = np.array(node_demand_data_tuples)
-    depot_xy = torch.FloatTensor(depot_xy_data).to(DEVICE)
-    node_xy = torch.FloatTensor(node_xy_data).to(DEVICE)
-    node_demand = torch.FloatTensor(node_demand_data)[:, :, None].to(DEVICE)  # unsqeeeze to match the shape of node_xy
-    return depot_xy, node_xy, node_demand, demand_scalers[0]
+    edge_ids_tuples, fractions_tuples, demands_tuples, cost_matrix_tuples = zip(*batch)
+    
+    # edge_ids must be LongTensor to serve as index tensors later
+    edge_ids = torch.LongTensor(np.array(edge_ids_tuples)).to(DEVICE)
+    fractions = torch.FloatTensor(np.array(fractions_tuples)).to(DEVICE)
+    demands = torch.FloatTensor(np.array(demands_tuples)).to(DEVICE)
+    cost_matrix = torch.FloatTensor(np.array(cost_matrix_tuples)).to(DEVICE)
+    
+    return edge_ids, fractions, demands, cost_matrix
 
-DATALOADER = CVRP_DATA_LOADER__RANDOM # short name
+
+DATALOADER = CVRP_DATA_LOADER # short name
+
+
+
+# ####################################
+# # DATA LOADER
+# ####################################
+# def CVRP_DATA_LOADER__RANDOM(num_sample: int, batch_size: int, 
+#                              problem_sizes_mean: Optional[List[NumberType]|NumberType]=None, 
+#                              problem_sizes_std: Optional[List[NumberType]|NumberType]=None, 
+#                              rng: Generator=None):
+#     ''' Data loader that generates random CVRP instances on-the-fly.
+#         The problem sizes (number of customers) are fixed per batch and sampled 
+#         from a gaussian with specified mean and std before rounding
+#         Parameters:
+#             num_sample: total number of data samples to generate
+#             batch_size: batch size for the data loader
+#             problem_sizes_mean: the means of problem size per batch; if None, set to 25
+#             problem_sizes_std: the stds of problem size per batch; if None, set to 0
+#             rng: random number generator for reproducibility
+#     '''
+#     # Input sanity checks
+#     n_batches = int(np.ceil(num_sample/batch_size)) # number of batches
+#     if isinstance(problem_sizes_mean, List):
+#         assert len(problem_sizes_mean) == n_batches, \
+#                 "Length of problem_sizes_mean must match the number of batches = ceil(num_sample/batch_size)"
+#         if isinstance(problem_sizes_std, List):
+#             assert len(problem_sizes_mean) == len(problem_sizes_std), \
+#                 "problem_sizes_mean and problem_sizes_std must have the same length if provided as lists"
+#     # Convert means and stds to list of length n_batches
+#     problem_sizes_mean = 25 if problem_sizes_mean is None else problem_sizes_mean
+#     problem_sizes_mean = [problem_sizes_mean]*n_batches if isinstance(problem_sizes_mean, NumberType) \
+#                             else problem_sizes_mean
+#     problem_sizes_std = 0 if problem_sizes_std is None else problem_sizes_std
+#     problem_sizes_std = [problem_sizes_std]*n_batches if isinstance(problem_sizes_std, NumberType) \
+#                             else problem_sizes_std
+#     assert np.all(np.array(problem_sizes_std)>=0), "problem_sizes_std must be nonnegative or contain nonnegative entries"
+    
+    
+#     rng = np.random.default_rng() if rng is None else rng
+#     dataset = CVRP_Dataset__Random(num_sample=num_sample, 
+#                                    batch_size=batch_size, 
+#                                    problem_sizes_mean=problem_sizes_mean,
+#                                    problem_sizes_std=problem_sizes_std,
+#                                    rng=rng)
+#     data_loader = DataLoader(dataset=dataset,
+#                              batch_size=batch_size,
+#                              shuffle=False,
+#                              num_workers=0,
+#                              collate_fn=CVRP_collate_fn)
+#     return data_loader
+
+# class CVRP_Dataset__Random(Dataset):
+#     def __init__(self, num_sample, batch_size, problem_sizes_mean, problem_sizes_std, rng=None):
+#         self.num_sample = num_sample
+#         self.batch_size = batch_size
+#         self.num_batches = int(np.ceil(num_sample / batch_size))
+#         self.rng = np.random.default_rng() if rng is None else rng
+
+#         # Generate dynamic problem size (possible number of customers) per batch
+#         # via guassian + clipping + rounding
+#         self.problem_size_list = np.round(self.rng.normal(problem_sizes_mean, problem_sizes_std))
+#         self.problem_size_list = np.clip(self.problem_size_list, MIN_NUM_CUSTOMERS, MAX_NUM_CUSTOMERS).astype(int)
+
+#         self.demand_scalers_list = [VEHICLE_CAPACITY] * len(self.problem_size_list)
+
+#     def __getitem__(self, index):
+#         # determine the problem size and demand scaler for this index
+#         batch_number = index // self.batch_size
+#         problem_size = self.problem_size_list[batch_number]
+#         demand_scaler = self.demand_scalers_list[batch_number]
+
+#         # generate random data
+#         depot_xy_data = self.rng.random((1, 2))
+#         node_xy_data = self.rng.random((problem_size, 2))
+#         node_demand_data = self.rng.integers(1, MAX_DEMAND_PER_CUSTOMER + 1, problem_size) / demand_scaler
+
+#         return depot_xy_data, node_xy_data, node_demand_data, demand_scaler
+
+#     def __len__(self):
+#         return self.num_sample
+
+
+# def CVRP_collate_fn(batch):
+#     depot_xy_data_tuples, node_xy_data_tuples, node_demand_data_tuples, demand_scalers = zip(*batch)
+#     depot_xy_data = np.array(depot_xy_data_tuples)
+#     node_xy_data = np.array(node_xy_data_tuples)
+#     node_demand_data = np.array(node_demand_data_tuples)
+#     depot_xy = torch.FloatTensor(depot_xy_data).to(DEVICE)
+#     node_xy = torch.FloatTensor(node_xy_data).to(DEVICE)
+#     node_demand = torch.FloatTensor(node_demand_data)[:, :, None].to(DEVICE)  # unsqeeeze to match the shape of node_xy
+#     return depot_xy, node_xy, node_demand, demand_scalers[0]
+
+# DATALOADER = CVRP_DATA_LOADER__RANDOM # short name
 
 ####################################
 # STATE
